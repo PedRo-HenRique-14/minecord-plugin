b16d8da9ec8ed1a1ea0707956e366d4b35b0cd9b
Dodas_ <ph005009004@gmail.com>
2023-09-20 18:21:46 -0300

    feat: Help menu when Arisia Bot is mentioned
    A embed is sent when someone mentions Arísia Bot

--- Minecord/src/main/java/org/dodopredo/minecord/bot/commands/BotMention.java
+++ Minecord/src/main/java/org/dodopredo/minecord/bot/commands/BotMention.java
@@ -3,6 +3,7 @@ package org.dodopredo.minecord.bot.commands;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.SelfUser;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.dodopredo.minecord.Minecord;
 
@@ -12,6 +13,7 @@ public class BotMention {
     public static void mentionEvent(MessageReceivedEvent event){
         if (!event.getMember().getUser().isBot()){
             Member member = event.getMember();
+            User arisiaBot = Minecord.getJda().getUserById(1077692558056112298L);
             SelfUser selfUser = Minecord.getJda().getSelfUser();
             if (event.getMessage().getContentRaw().trim().equals(selfUser.getAsMention())){
                 EmbedBuilder embed = new EmbedBuilder();
@@ -23,6 +25,16 @@ public class BotMention {
                 embed.addField("Se tiver alguma dúvida...", "[Acesse a minha wiki aqui!](https://github.com/PedRo-HenRique-14/Minecord-Wiki)", false);
                 embed.setColor(new Color(68, 68, 68));
                 embed.setThumbnail(selfUser.getAvatarUrl());
+                event.getChannel().sendMessageEmbeds(embed.build()).queue();
+            }
+
+            if (event.getMessage().getContentRaw().trim().equals(arisiaBot.getAsMention())){
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setAuthor(member.getUser().getName(), member.getEffectiveAvatarUrl(),member.getEffectiveAvatarUrl());
+                embed.setTitle("Opa %s, bão?", member.getEffectiveName());
+                embed.setDescription("Está precisando de ajuda com o código? Se a resposta for não, pode ignorar esta mensagem. Mas se a resposta for sim, eu vou te explicar como funciona. Faz assim, vai no **privado** do %s e envia esse `código` que apareceu na sua tela quando você tentou entrar em Arísia. Depois de enviar o código, você estará livre para jogar! Se tiver qualquer dúvida, não exite em perguntar.");
+                embed.setColor(new Color(68, 68, 68));
+                embed.setThumbnail(arisiaBot.getAvatarUrl());
                 event.getChannel().sendMessageEmbeds(embed.build()).queue();
             }
         }
