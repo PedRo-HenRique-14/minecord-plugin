b9172b79c4eaf52f18eddc26044ddb2544e1350c
Dodas_ <ph005009004@gmail.com>
2023-09-20 14:22:29 -0300

    refactor(ChatBroadcast,java)
    New method sendToDiscordEmbed

--- Minecord/src/main/java/org/dodopredo/minecord/utils/ChatBroadcast.java
+++ Minecord/src/main/java/org/dodopredo/minecord/utils/ChatBroadcast.java
@@ -1,9 +1,13 @@
 package org.dodopredo.minecord.utils;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.dodopredo.minecord.Minecord;
+
+import java.awt.*;
 
 public class ChatBroadcast {
     public static void sendToMinecraft(MessageReceivedEvent event){
@@ -11,7 +15,7 @@ public class ChatBroadcast {
         try {
             if (!event.getMember().getUser().isBot()) {
                 if (Minecord.DISCORD_TO_MINECRAFT_ENABLE) {
-                    if (event.getChannel().equals(Minecord.getJda().getTextChannelById(Minecord.DISCORD_TO_MINECRAFT_CHANNEL))) {
+                    if (event.getChannel().equals(Minecord.getJda().getTextChannelById(Minecord.DISCORD_TO_MINECRAFT_CHANNEL_ID))) {
                         // Sempre que uma mensagem for enviada no canal DISCORD_TO_MINECRAFT_CHANNEL, ela será transmitida para o chat do jogo
 
                         String userName = event.getAuthor().getEffectiveName();
@@ -24,7 +28,31 @@ public class ChatBroadcast {
         } catch (NullPointerException e) {}
 
     }
-    public static void sendToDiscord(){
+    public static void sendToDiscordEmbed(long channelId, String title, String description, Color color, Player author, String thumbURL){
+        try {
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setTitle(title);
+            embedBuilder.setDescription(description);
+            if (color != null){
+                embedBuilder.setColor(color);
+            }
+            if (author != null){
+                embedBuilder.setAuthor(author.getDisplayName());
+            }
+            if (thumbURL != null){
+                embedBuilder.setThumbnail(thumbURL);
+            }
+            Minecord.getJda().getTextChannelById(channelId).sendMessageEmbeds(embedBuilder.build()).queue();
+        } catch (NullPointerException e){
+            // oof
+        }
+    }
 
+    public static void sendToDiscord(long channelId, String message){
+        try {
+            Minecord.getJda().getTextChannelById(channelId).sendMessage(message);
+        } catch (NullPointerException e){
+            // oof
+        }
     }
 }
